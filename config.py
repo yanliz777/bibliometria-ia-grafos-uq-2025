40a15ebeefeedaf60f6e3fc96508c6612556fa7e
repoYan <yan_cristiano@hotmail.com>
2025-11-08 21:28:56 -0500

    Modifique el archivo config

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ URL_LOGIN = "https://library.uniquindio.edu.co/databases"
 
 # Credenciales UQ
 USUARIO = "yfgomezc@uqvirtual.edu.co"
-CONTRASENA = "***"
+CONTRASENA = "****"
 
 # URL ScienceDirect vía CRAI
 SCIENCEDIRECT_URL = "https://www-sciencedirect-com.crai.referencistas.com/"
